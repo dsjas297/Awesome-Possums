cd44c078d1b62ec5dfb10e1d3421127ff4ebdb3f
Sohan Jain <sj346@cornell.edu>
2012-03-09 18:37:26 -0500

    serve static files from node

--- server.js
+++ server.js
@@ -1,8 +1,47 @@
-var html = require('fs').readFileSync(__dirname+'/content/test.html');
-var server = require('http').createServer(function(req, res){
-  res.end(html);
-});
-server.listen(8080);
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+ 
+http.createServer(function (request, response) {
+ 
+    console.log('request starting...');
+     
+    var filePath = '.' + request.url;
+    if (filePath == './')
+        filePath = './index.htm';
+         
+    var extname = path.extname(filePath);
+    var contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+    }
+     
+    path.exists(filePath, function(exists) {
+     
+        if (exists) {
+            fs.readFile(filePath, function(error, content) {
+                if (error) {
+                    response.writeHead(500);
+                    response.end();
+                }
+                else {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                }
+            });
+        }
+        else {
+            response.writeHead(404);
+            response.end();
+        }
+    });
+     
+}).listen(8080);
 
 var nowjs = require("now");
 var everyone = nowjs.initialize(server);
